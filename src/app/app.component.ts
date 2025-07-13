c08d349f47040460cb89132af20aed60e9a8a6df
Nick <n.sakharov@icloud.com>
2025-07-13 15:36:20 +0700

    Benefits section done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Hero2Component } from './hero2/hero2.component';
+import { BenefitsComponent } from './benefits/benefits.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroComponent, NavbarComponent, Hero2Component],
+  imports: [
+    RouterOutlet,
+    HeroComponent,
+    NavbarComponent,
+    Hero2Component,
+    BenefitsComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
